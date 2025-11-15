f9f0585561316749892b293945617af9debbcaab
Darsh20009 <youssefdarwish20009@gmail.com>
2025-11-16 01:21:18 +0300

    تعديل جديد ي قسم الاشترات

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
     ],
   },
   allowedDevOrigins: ['*'],
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 };
 
 export default nextConfig;
